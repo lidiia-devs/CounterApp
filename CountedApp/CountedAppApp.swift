96fbb7c2aba1af63dff3324fc9b3153baf270492
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-02-10 21:04:45 -0500

    Initial Commit

---
+++ CountedApp/CountedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountedAppApp.swift
+//  CountedApp
+//
+//  Created by Lidiia Diachkovskaia on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CountedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
